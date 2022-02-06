a389582b655aab1bc44d930031527255ad2e8305
nika <nikakalandias@gmail.com>
2022-02-06 19:44:17 +0300

    Finished

--- Progressbar-App/ViewController.swift
+++ Progressbar-App/ViewController.swift
@@ -9,11 +9,54 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var buttonOutlet: UIButton!
+    @IBOutlet weak var progressLabel: UILabel!
+    @IBOutlet weak var progressbar: UIProgressView!
+    @IBOutlet weak var compImage: UIImageView!
+    
+    let maxTime : Float = 100.0
+    var currentTime : Float = 0.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Açılışta fotoğrafı gizledik
+        compImage.isHidden = true
+        
+        // Arkaplanı kodla yaptık (daha iyi)
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "background.png")!)
+        
+        // Butonlara shape ekledik
+        buttonOutlet.layer.cornerRadius = 17
+        buttonOutlet.layer.masksToBounds = true
+        
+        progressbar.setProgress(0, animated: true)
     }
-
+    
+    @IBAction func buttonAction(_ sender: Any) {
+        compImage.isHidden = true
+        currentTime = 0
+        progressLabel.text = "%\(Int(currentTime))"
+        progressbar.setProgress(0, animated: true)
+        perform(#selector(progressUpdate), with: nil, afterDelay: 1.0)
+    }
+    
+    @objc func progressUpdate() {
+        
+        currentTime = currentTime + 1
+        let deger = currentTime / 100
+        
+        progressbar.setProgress(deger, animated: true)
+        
+        if currentTime < maxTime {
+            progressLabel.text = "%\(Int(currentTime))"
+            perform(#selector(progressUpdate), with: nil, afterDelay: 0.05)
+        }else {
+            compImage.isHidden = false
+            progressLabel.text = "Completed!"
+        }
+    }
+    
 
 }
 
